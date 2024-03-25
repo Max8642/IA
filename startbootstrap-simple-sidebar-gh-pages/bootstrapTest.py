243f29e0a744ab6920f0534faf59000626fe014e
Max8642 <memasri101@gmail.com>
2024-03-24 23:02:59 -0400

    Table display using url for event code

--- startbootstrap-simple-sidebar-gh-pages/bootstrapTest.py
+++ startbootstrap-simple-sidebar-gh-pages/bootstrapTest.py
@@ -1,11 +1,19 @@
+import flask
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+#@app.route("/")
+#def home():
+ #   return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+ #   app.run(debug=True)
+
+@app.route('/')
+def index():
+   #return render_template('index.html')  # this line only displays files in the templates sub directory.  To display from other directories, see below
+   return flask.send_from_directory(".", path="index.html")
+if __name__ == '__main__':
+   app.run()
